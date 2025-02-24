7b10f162bd0edc4cc26c65a715fabe33265452ca
Vishnu24o <kawalevishnu360@gmail.com>
2025-02-24 12:24:28 +0530

    Create while loop.py

---
+++ while loop.py
@@ -0,0 +1,12 @@
+b=input("enter your name : ")
+while(1):
+ a=input("enter your choice: \n 1) wedding \n 2) party \n ")
+
+ if(a=="wedding"):
+    print(f"Kaha khake aana {b}")
+    break
+ elif(a=="party"):
+    print(f"Daru Mat Pina {b}")
+    break
+ else:
+    print(f"incorrect choice / Enter your choice again {b}")
