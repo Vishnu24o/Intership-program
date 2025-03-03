0c16e249b1e393e19883a09a3cdba0fe76603444
Vishnu24o <kawalevishnu360@gmail.com>
2025-03-03 22:03:10 +0530

    Create for loop in nested.py

---
+++ for loop in nested.py
@@ -0,0 +1,17 @@
+# for loop in nested (the outer loop will perform only one variable at a time and the inner loop will perform the whole string with the outer loop first variable)
+
+
+p=['a','b','c','d','e']
+q=[1,2,3,4,5]
+for f in p:
+    for g in q:
+        print (f,g)
+    
+
+
+
+w=[1,2,3,4,5,6,7]
+l=['a','d','r','t']
+for f in w:
+    for v in l:
+        print(f,v)
