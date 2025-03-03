60372c5169b4ea471036831f9c9f02b81f5eb9bd
Vishnu24o <kawalevishnu360@gmail.com>
2025-03-03 21:48:46 +0530

    Create star pattern using for loop.py

---
+++ star pattern using for loop.py
@@ -0,0 +1,19 @@
+a = 10
+for i in range(a):
+    for c in range(i+1):
+        print('*',end='')
+    print('')
+
+
+
+
+
+
+# infinte star pattern using while loop (for practice)
+
+import time
+a=1
+while(1):
+    print('*'* a)
+    a+=1
+    time.sleep(1)
