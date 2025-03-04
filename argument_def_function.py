cec0b300cf6f34fa09dfe3f12d8c5dce8c79b866
Vishnu24o <kawalevishnu360@gmail.com>
2025-03-05 00:08:39 +0530

    Create argument_def_function.py

---
+++ argument_def_function.py
@@ -0,0 +1,12 @@
+# here we are argumenting to function with our input value
+def add(a,b):
+    c=a+b
+    print(c)
+
+add(10,20)
+
+# here we are calling the file 
+import normal_def_function
+
+# only calling the function
+from normal_def_function import abc
